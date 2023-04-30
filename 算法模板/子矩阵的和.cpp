697d6f31956560e71d6efa0ad83366bcf0a0313f
qys <94467246+qqyyss@users.noreply.github.com>
2023-05-01 00:25:42 +0800

    子矩阵的和

---
+++ 算法模板/子矩阵的和.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+const int N = 1010;
+
+int a[N][N],s[N][N];
+
+
+int main()
+{
+    int n,m,q;
+    cin >> n>> m>>q;
+    for(int i = 1;i <=n ;i++ )
+        for(int j = 1;j <= m;j++)
+            cin >> a[i][j];
+            
+    for(int i = 1;i <= n;i++)
+        for(int j = 1;j <= m;j++)
+            s[i][j] = s[i-1][j] + s[i][j-1] - s[i-1][j-1] + a[i][j];
+            
+    while(q--)
+    {
+        int x1,y1,x2,y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        cout << s[x2][y2] - s[x1-1][y2]  - s[x2][y1 - 1] + s[x1 -1][y1 - 1] <<endl;
+    }
+    return 0;
+}
