510a798ec456f076a0c2277e701dfcecf1383a64
qys <94467246+qqyyss@users.noreply.github.com>
2023-05-08 23:58:21 +0800

    n皇后问题

--- 算法模板/n皇后问题.cpp
+++ 算法模板/n皇后问题.cpp
@@ -39,3 +39,51 @@ int main()
     dfs(0);
     return 0;
 }
+
+
+
+// 解法2
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+const int N = 20;
+int n;
+char g[N][N];
+bool row[N],col[N],dg[N],udg[N];
+
+void dfs(int x,int y,int s)
+{
+    if(y == n) y = 0 ,x++;
+    
+    if(x == n)
+    {
+        if(s == n)
+        {
+            for(int i = 0;i < n;i++) puts(g[i]);
+            puts("");
+        }
+         return;
+    }
+    dfs(x,y+1,s);
+    
+    if(!row[x] && !col[y] && !dg[x + y] && !udg[x - y + n])
+    {
+        g[x][y] = 'Q';
+        row[x] = col[y] = dg[x + y] = udg[x - y + n] = true;
+        dfs(x,y+1,s+1);
+        row[x] = col[y] = dg[x + y] = udg[x - y + n] = false;
+        g[x][y] = '.';
+    }
+}
+
+int main()
+{
+    cin >> n;
+    for(int i = 0;i < n;i++)
+        for(int j = 0;j < n;j++)
+            g[i][j] = '.';
+    dfs(0,0,0);
+    return 0;
+}
